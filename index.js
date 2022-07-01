32044594a7692d15fd08c6ca30740faf5629a0da
shawon-MG <work.shawomg@gmail.com>
2022-07-01 08:13:59 +0600

    mongoDB setted up

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
 app.use(
@@ -16,6 +17,28 @@ app.use(
     })
 );
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jshb2ox.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const toDoListCollection = client.db('demo-job-task--project').collection('to-do-lists');
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+// Root API:---------------------------------------------------------
 app.get('/', (req, res) => {
     res.send("End Game Task-Project's server is live");
 });
